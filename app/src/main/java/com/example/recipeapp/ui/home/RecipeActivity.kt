df746e5f1adfc10ea7b21d32a367072443e97fee
tech faz <amanymohamedmorsy23@gmail.com>
2025-08-09 17:11:22 +0200

    add option menu

--- app/src/main/java/com/example/recipeapp/ui/home/RecipeActivity.kt
+++ app/src/main/java/com/example/recipeapp/ui/home/RecipeActivity.kt
@@ -1,21 +1,72 @@
 package com.example.recipeapp.ui.home
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.recipeapp.R
+import com.example.recipeapp.databinding.ActivityRecipeBinding
 
 class RecipeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRecipeBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_recipe)
+        binding = ActivityRecipeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_recipe) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.searchFragment, R.id.favoriteFragment)
+        )
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_about -> {
+                navController.navigate(R.id.aboutFragment)
+                true
+            }
+            R.id.action_sign_out -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+
+
 }
